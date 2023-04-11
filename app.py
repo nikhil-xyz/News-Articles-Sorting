3d7725f6a2a3dd8d083f756b5bcd661f43b492c5
Nikhil Sonkusare <sonkusarenikhil@sggs.ac.in>
2023-04-11 17:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,27 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 import gradio as gr
-from sklearn.ensemble import RandomForestClassifier
+import logging
+
+# initializing logging
+logging.basicConfig(filename='logs.log',
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s - %(message)s)',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.DEBUG)
 
 # generating object for the lemmatizer
-wnl = WordNetLemmatizer()
+try:
+    wnl = WordNetLemmatizer()
+except:
+    logging.debug("Problem while invoking WordNetLemmatizer object")
 
 # loading model and vectorizer....
-model = pickle.load(open('model.pkl', 'rb'))
-vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+try:
+    model = pickle.load(open('model.pkl', 'rb'))
+    vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+except:
+    logging.debug("Problem while loading pickle files")
 
 # preprocessing: remove stopwords, lemmatize, remove non-strings, vectorized the text, predict the output and return
 def process(text):
@@ -23,9 +36,11 @@ def process(text):
     text = text.split()
     text = [wnl.lemmatize(word) for word in text if not word in stopwords.words('english')]
     text = ' '.join(text)
-
-    text = vectorizer.transform([text]).toarray()
-    result = model.predict(text)[0]
+    try:
+        text = vectorizer.transform([text]).toarray()
+        result = model.predict(text)[0]
+    except:
+        logging.debug("Problem while vectorization and prediction")
     return result
 
 # Gradio interface
@@ -34,6 +49,6 @@ interface = gr.Interface(fn=process,
                 title = title,
                 inputs=gr.inputs.Textbox(lines=20, placeholder='Insert the News Article'),
                 outputs='text')
-interface.launch(share=True)
+interface.launch(server_name="0.0.0.0", server_port=7860)
 
 
